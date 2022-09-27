d5e0493ed4230af6d8d0a010baeae8f1d317956b
Omar Mahat <omar.mahat@student.moringaschool.com>
2022-09-27 19:23:37 +0300

    commit tree ace22d66d4c2a7fb9aa0331b3a21150e45319a45

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,7 +1,12 @@
 class ReviewsController < ApplicationController
 
   def index
-    reviews = Review.all.order(rating: :desc)
+    if params[:dog_house_id]
+      dog_house = DogHouse.find(params[:dog_house_id])
+      reviews = dog_house.reviews
+    else
+      reviews = Review.all
+    end
     render json: reviews, include: :dog_house
   end
 
